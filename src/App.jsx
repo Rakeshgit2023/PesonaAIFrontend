c68afd3e3d217a16dd9e6824d5397e36be6ca661
RakeshMagic2025 <rakesh.shaw@magicworkshop.ai>
2025-08-20 03:57:13 +0530

    added hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import ContextProvider from "./context/ContextProvider";
 import ParentRoute from "./route/ParentRoute";
 const App = () => {
